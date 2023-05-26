a42725e5ef39fcc3418b0aa21d6488ef92975641
Adriana Kopp <acm.kopp@gmail.com>
2023-05-26 14:39:09 +0200

    context

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import { CartContainer } from "./components/CartContainer/CartContainer";
+
 // importación de Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { CartContext } from "./context/CartContext";
+
 function App() {
   return (
-    <div>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<ItemListContainer />} />
-          <Route path="/category/:categoryId" element={<ItemListContainer />} />
-          <Route path="/detail/:itemId" element={<ItemDetailContainer />} />
-          <Route path="*" element={<h1>404 NOT FOUND</h1>} />
-          <Route path="/cart" element={<CartContainer />} />
-          <Route path="*" element={<Navigate to={"/"} />} />
-        </Routes>
-      </Router>
-    </div>
+    <CartContext>
+      <div>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route
+              path="/category/:categoryId"
+              element={<ItemListContainer />}
+            />
+            <Route path="/detail/:itemId" element={<ItemDetailContainer />} />
+            <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+            <Route path="/cart" element={<CartContainer />} />
+            <Route path="*" element={<Navigate to={"/"} />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </CartContext>
   );
 }
 
